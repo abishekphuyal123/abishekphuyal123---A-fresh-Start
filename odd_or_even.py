c384f90af3e6ce6e8a9de69b19ed0cc6f6bf8940
Abishek Phuyal <66534961+abishekphuyal123@users.noreply.github.com>
2023-03-17 13:01:57 +0000

    Even-Odd Generator.py

---
+++ odd_or_even.py
@@ -0,0 +1,7 @@
+# A simple program, that checks whether the entered number is odd or even. 
+number = int(input("Enter a number: \n"))
+if number % 2 == 0: 
+    print(f"The given number {number} is even.")
+else:
+    print(f"The given number {number} is odd.")
+

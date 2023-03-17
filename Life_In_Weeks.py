c5041ad4776a67d6f0b15cc303bae4fbde83cfb3
Abishek Phuyal <66534961+abishekphuyal123@users.noreply.github.com>
2023-03-17 12:27:06 +0000

    My First commit.

---
+++ Life_In_Weeks.py
@@ -0,0 +1,8 @@
+"""Create a program using maths and f-strings, that tells us how many days, months, weeks, we have left if 
+we live until the age of 90 years. """
+# A year has 365 Days, 52 Weeks & 12 Months.
+age= int(input("Enter your age. "))
+months = (90-age) * 12
+weeks = (90 - age)*52
+Days = (90 - age)*365
+print(f"You have {months} months, {weeks} weeks, and {Days} days to live in this world.")

49c64058b4a12339f0029353c0db0a5c1ea22b77
Abishek Phuyal <66534961+abishekphuyal123@users.noreply.github.com>
2023-04-07 13:11:25 +0000

    Python function with outputs

--- Day - 10 Functions with Outputs/1.py
+++ Day - 10 Functions with Outputs/1.py
@@ -2,6 +2,6 @@ def name_function(f_name, l_name):
     first = f_name.title()
     last = l_name.title()
     return f"{first} {last}."
-
+    print("Hello World. ")
 abi = name_function("Abishek","Phuyal")
 print(abi)

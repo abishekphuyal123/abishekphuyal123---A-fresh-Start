cef8bbaae84e72b6c1103b19fb2ee7a98face713
Abishek Phuyal <66534961+abishekphuyal123@users.noreply.github.com>
2023-04-11 22:37:50 +0000

    Modifying Global Scope & using it inside a function in python

--- Day - 12 Scope & Number Guessing Game/Modifying_scope.py
+++ Day - 12 Scope & Number Guessing Game/Modifying_scope.py
@@ -10,3 +10,13 @@ increase_enemies()
 
 # The variable enemies = 1, declared outside the function increase_enemies() is assigned a value of 1, and the value is increased by 1 inside the increase_enemies() function. 
 # Thus, in order to use a global variable, inside of a function, we need to define it explicitly using the global statement. 
+
+print()
+
+animal = "dog"
+print(animal)
+
+def print_animal():
+    global animal
+    print(animal)
+print_animal()

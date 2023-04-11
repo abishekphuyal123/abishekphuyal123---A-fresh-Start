eb6e43e861fb2a99fe4cc08dbe3f144939d40fff
Abishek Phuyal <66534961+abishekphuyal123@users.noreply.github.com>
2023-04-11 22:46:11 +0000

    Global Variable Naming Scope

---
+++ Day - 12 Scope & Number Guessing Game/Naming_scopes.py
@@ -0,0 +1,10 @@
+# In order to differentiate global scopes from variables, we use a different naming convention. i.e. We use uppercase letters to declare a global variables. 
+
+# Global Variables are very useful, while declaring  constants whose value remain the same throughout the course of the program. 
+
+# For Example:
+
+PI = 3.14159
+URL = "https://www.google.com"
+print(PI)
+print(URL)
